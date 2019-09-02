pmotschmann/Evolve

commit e06945e06bfa4f6f2c2d0fe59e8701a100dc2b01

Fix some things

space.js
@@ -1548,8 +1548,8 @@ const interstellarProjects = {
         },
         alpha_mission: {
             id: 'interstellar-alpha_mission',
-            title: loc('interstellar_alpha_mission'),
-            desc: loc('interstellar_alpha_mission'),
+            title: loc('space_mission_title', ['interstellar_alpha_name']),
+            desc: loc('space_mission_desc', ['interstellar_alpha_name']),
             reqs: { ftl: 2 },
             grant: ['alpha',1],
             no_queue(){ return global.queue.queue.some(item => item.id === $(this)[0].id) ? true : false; },
@@ -1941,8 +1941,8 @@ const interstellarProjects = {
         },
         proxima_mission: {
             id: 'interstellar-proxima_mission',
-            title: loc('interstellar_proxima_mission'),
-            desc: loc('interstellar_proxima_mission'),
+            title: loc('space_mission_title',['interstellar_proxima_name']),
+            desc: loc('space_mission_desc',['interstellar_proxima_name']),
             reqs: { alpha: 1 },
             grant: ['proxima',1],
             no_queue(){ return global.queue.queue.some(item => item.id === $(this)[0].id) ? true : false; },
@@ -2106,8 +2106,8 @@ const interstellarProjects = {
         },
         nebula_mission: {
             id: 'interstellar-nebula_mission',
-            title: loc('interstellar_nebula_mission'),
-            desc: loc('interstellar_nebula_mission'),
+            title: loc('space_mission_title',['interstellar_nebula_name']),
+            desc: loc('space_mission_desc',['interstellar_nebula_name']),
             reqs: { alpha: 1 },
             grant: ['nebula',1],
             no_queue(){ return global.queue.queue.some(item => item.id === $(this)[0].id) ? true : false; },
@@ -2232,8 +2232,8 @@ const interstellarProjects = {
         },
         neutron_mission: {
             id: 'interstellar-neutron_mission',
-            title: loc('interstellar_neutron_mission'),
-            desc: loc('interstellar_neutron_mission'),
+            title: loc('space_mission_title', ['interstellar_neutron_name']),
+            desc: loc('space_mission_desc', ['interstellar_neutron_name']),
             reqs: { nebula: 1, high_tech: 14 },
             grant: ['neutron',1],
             no_queue(){ return global.queue.queue.some(item => item.id === $(this)[0].id) ? true : false; },
@@ -2300,8 +2300,8 @@ const interstellarProjects = {
         },
         blackhole_mission: {
             id: 'interstellar-blackhole_mission',
-            title: loc('interstellar_blackhole_mission'),
-            desc: loc('interstellar_blackhole_mission'),
+            title: loc('space_mission_title', ['interstellar_blackhole_name']),
+            desc: loc('space_mission_desc', ['interstellar_blackhole_name']),
             reqs: { nebula: 1 },
             grant: ['blackhole',1],
             no_queue(){ return global.queue.queue.some(item => item.id === $(this)[0].id) ? true : false; },

locale.js
@@ -45,9 +45,7 @@ function getString(locale) {
             $.getJSON(`strings/strings.${locale}.json`, (data) => { localeString = data; })
         }
         catch (e) {
-            if (locale != 'en-US') {
-                console.error(e,e.stack);
-            }
+            console.error(e,e.stack);
         }
         const defSize = defaultString.length;
 

GodofTheFallen/Evolve-zh-CN-Localization

commit 6b4941bb1a3d77c98e3c5bd03beb79482e342692

String fixes
Fixed strings broken by e06945e06bfa4f6f2c2d0fe59e8701a100dc2b01

space.js
@@ -1548,8 +1548,8 @@ const interstellarProjects = {
         },
         alpha_mission: {
             id: 'interstellar-alpha_mission',
-            title: loc('space_mission_title', ['interstellar_alpha_name']),
-            desc: loc('space_mission_desc', ['interstellar_alpha_name']),
+            title: loc('space_mission_title', [loc('interstellar_alpha_name')]),
+            desc: loc('space_mission_desc', [loc('interstellar_alpha_name')]),
             reqs: { ftl: 2 },
             grant: ['alpha',1],
             no_queue(){ return global.queue.queue.some(item => item.id === $(this)[0].id) ? true : false; },
@@ -1941,8 +1941,8 @@ const interstellarProjects = {
         },
         proxima_mission: {
             id: 'interstellar-proxima_mission',
-            title: loc('space_mission_title',['interstellar_proxima_name']),
-            desc: loc('space_mission_desc',['interstellar_proxima_name']),
+            title: loc('space_mission_title',[loc('interstellar_proxima_name')]),
+            desc: loc('space_mission_desc',[loc('interstellar_proxima_name')]),
             reqs: { alpha: 1 },
             grant: ['proxima',1],
             no_queue(){ return global.queue.queue.some(item => item.id === $(this)[0].id) ? true : false; },
@@ -2106,8 +2106,8 @@ const interstellarProjects = {
         },
         nebula_mission: {
             id: 'interstellar-nebula_mission',
-            title: loc('space_mission_title',['interstellar_nebula_name']),
-            desc: loc('space_mission_desc',['interstellar_nebula_name']),
+            title: loc('space_mission_title',[loc('interstellar_nebula_name')]),
+            desc: loc('space_mission_desc',[loc('interstellar_nebula_name')]),
             reqs: { alpha: 1 },
             grant: ['nebula',1],
             no_queue(){ return global.queue.queue.some(item => item.id === $(this)[0].id) ? true : false; },
@@ -2300,8 +2300,8 @@ const interstellarProjects = {
         },
         blackhole_mission: {
             id: 'interstellar-blackhole_mission',
-            title: loc('space_mission_title', ['interstellar_blackhole_name']),
-            desc: loc('space_mission_desc', ['interstellar_blackhole_name']),
+            title: loc('space_mission_title', [loc('interstellar_blackhole_name')]),
+            desc: loc('space_mission_desc', [loc('interstellar_blackhole_name')]),
             reqs: { nebula: 1 },
             grant: ['blackhole',1],
             no_queue(){ return global.queue.queue.some(item => item.id === $(this)[0].id) ? true : false; },